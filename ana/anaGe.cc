ffecc14dddfc1fd7d54625094953d042dddc8293
liebercanis <mgold@unm.edu>
2020-02-26 14:01:54 -0700

    anaGe for analysis of LTree

--- ana/anaGe.cc
+++ ana/anaGe.cc
@@ -13,6 +13,8 @@
 #include "TSystemDirectory.h"
 #include "MGTMCEventSteps.hh"
 #include "MGTMCEventHeader.hh"
+#include "TLArEvent.hxx"
+#include "TGeEvent.hxx"
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -21,44 +23,6 @@
 enum {NDET=2000};
 
 using namespace std;
-
-  // for LAr
-  struct lArEvent_t {
-    double edep;
-    double PE;
-    double PVx;
-    double PVy;
-    double PVz;
-    double e0;
-    double x0;
-    double y0;
-    double z0;
-    double xf;
-    double yf;
-    double zf;
-    int ev;
-    int id;
-  };
-  lArEvent_t lArEvent;
-
-
-/*
-struct lArEvent_t {
-  double Energy;
-  double PE;
-};
-lArEvent_t lArEvent;
-*/
-void printlArEvent() {
-    TString mess;
-    mess.Form(" \t ***** lArEvent  **** %d (%.1f,%.1f,%.1f)",lArEvent.ev,lArEvent.PVx,lArEvent.PVy,lArEvent.PVz);
-    cout << mess << endl;
-    mess.Form(" \t pid %i e0 %.3f first (%.1f,%.1f,%.1f) last (%.1f,%.1f,%.1f) " ,lArEvent.id,lArEvent.e0,
-        lArEvent.x0,lArEvent.y0,lArEvent.z0,lArEvent.xf,lArEvent.yf,lArEvent.zf);
-    cout << mess << endl;
-    mess.Form(" \t edep %.3f PE %.3f",lArEvent.edep,lArEvent.PE);
-    cout << mess << endl;
-  }
 
 
 //sensVolID = 1000000 + columnNo*100 + crystalNo encoded in LGND_200_14String.cc
@@ -97,71 +61,58 @@ int main(int argc, char *argv[])
   TH2I *hOcc = new TH2I("Occ","occupancy",20,0,20,10,0,10);
   TH1D *hEnergyLAr = new TH1D("EnergyLAr","Energy in LAr",2000,0,6);
   TNtuple *ntEvent = new TNtuple("ntEvent"," event variables ","ev:ege:elar:npe");
+  TNtuple *ntGe = new TNtuple("ntGe"," ge hit variables ","ev:ihit:time:edep:x:y:z");
   hOcc->SetXTitle("string");
   hOcc->SetYTitle("unit");
   for(unsigned ih=0; ih<NDET; ++ih) {
     hEnergy[ih]= new TH1D(Form("Energy-det%u",ih),Form("Event Energy det %u",ih),2000,0,6);
   }
-  TChain *fChain=new TChain("fTree");
-  TChain *aChain=new TChain("fATree");
+  TChain *fChain=new TChain("LTree");
   
-  fChain->Add("/data2/mgold/MaGe_data/TlSource31-18-12-2019.root");
-  aChain->Add("/data2/mgold/MaGe_data/TlSource31-18-12-2019.root");
-  fChain->Add("/data2/mgold/MaGe_data/TlSource32-18-12-2019.root");
-  aChain->Add("/data2/mgold/MaGe_data/TlSource32-18-12-2019.root");
-  fChain->Add("/data2/mgold/MaGe_data/TlSource33-18-12-2019.root");
-  aChain->Add("/data2/mgold/MaGe_data/TlSource33-18-12-2019.root");
-  printf(" fTree %llu ATree %llu \n", fChain->GetEntries(),aChain->GetEntries());
+  fChain->Add("data/TlSource-test31.root");
+  printf(" LTree %llu \n", fChain->GetEntries());
   //MGDO classes
   MGTMCEventSteps *eventSteps = NULL;
   MGTMCEventHeader *eventHeader = NULL;
   //MGTMCStepData *step = NULL;
-  fChain->SetBranchAddress("eventSteps", &eventSteps);
-  fChain->SetBranchAddress("eventHeader",&eventHeader);
-  aChain->SetBranchAddress("lArEvent",&lArEvent);
-  if(fChain->GetEntries()!= aChain->GetEntries()) {
-    printf(" %llu is not = %llu \n ",fChain->GetEntries(),aChain->GetEntries());
+  // Legend Tree
+  TLArEvent *larEvent;
+  TGeEvent *geEvent;
+  fChain->SetBranchAddress("LArEvent",&larEvent);
+  fChain->SetBranchAddress("GeEvent",&geEvent);
+  if(fChain->GetEntries()!= fChain->GetEntries()) {
+    printf(" %llu is not = %llu \n ",fChain->GetEntries(),fChain->GetEntries());
     return 1;
   }
-  printf(" fTree %llu ATree %llu ", fChain->GetEntries(),aChain->GetEntries());
-  aChain->Print();
+  printf(" fTree %llu ATree %llu ", fChain->GetEntries(),fChain->GetEntries());
+  fChain->Print();
   
-  Long64_t nentries = aChain->GetEntries();
+  std::map<double, TGeHit>::iterator gIter;
+  Long64_t nentries = fChain->GetEntries();
   for(Long64_t entry=0 ; entry<nentries; ++entry) {
     fChain->GetEntry(entry);  
-    aChain->GetEntry(entry);
-    hEnergyLAr->Fill(lArEvent.edep);
+    hEnergyLAr->Fill(larEvent->eLAr);
+    if(entry%1==0) printf(" ... event %llu geDets %lu larHits %lu \n",entry, geEvent->geDet.size(),larEvent->hits.size());
 
-    std::map<int,double> emap = eventHeader->GetEnergyMap();
-    //if( lArEvent.id!=11) printf(" NOT e- entry %llu energy %f pe %f ID %i \n",entry,lArEvent.edep,lArEvent.PE,int(lArEvent.id));
-    if(entry%1000==0) {
-      //printf(" ... event %llu map size %lu \n",entry,emap.size());
-      printf(" ... event %llu \n",entry);
-      printlArEvent();
-      //unsigned idcount=0;
-      //for (std::map<int,double>::iterator idIter=emap.begin() ; idIter!=emap.end(); ++idIter)
-      //  printf(" id %u first %i (%i)  second %E \n",idcount++, idIter->first, int(idIter->first)-1010000 , idIter->second);
-    }
-
+    // loop over ge dets
     double eGe=0;
-
-    for (std::map<int,double>::iterator idIter=emap.begin() ; idIter!=emap.end(); ++idIter) {
-      if(idIter->first>1000000) {
-        int iarray, istring, iunit, idet;
-        decode(idIter->first,iarray,istring,iunit,idet);
-        //printf(" array %i istring %i unit = %i idet = %i\n",iarray,istring,iunit,idet);
-        hOcc->Fill(istring,iunit);
-        if(idet>0&&idet<2000) {
-          eGe += idIter->second;
-          hEnergy[idet]->Fill(idIter->second);
-          hEventEnergy->Fill(idIter->second);
-          if(lArEvent.PE<1) hEventEnergyCut->Fill(idIter->second); 
-        }
-        else 
-          printf(" ERROR!! illegal idet %i \n",idet);
+    for(unsigned idet =0; idet < geEvent->geDet.size(); ++idet) {
+      TGeDet *gdet = &geEvent->geDet[idet];
+      printf(" \t idet %u hits %lu \n",idet,gdet->hitList.size());
+      gIter=gdet->hitList.begin();
+      unsigned icount=0;
+      for(std::map<double, TGeHit>::iterator gIter= gdet->hitList.begin(); gIter!= gdet->hitList.end(); ++gIter ) {
+        TGeHit *ghit = &gIter->second;
+        Double_t edep =  ghit->eDep;  
+        Double_t time = ghit->time;
+        TVector3 local = ghit->local;
+        printf(" \t\t idet %u ihit %u  edep %E time %E \n",idet,icount++,edep,time);
+        eGe += edep;
+        hEventEnergy->Fill(eGe);
+        ntGe->Fill(float(entry),float(icount),time,edep,local.X(),local.Y(),local.Z());
       }
     }
-    ntEvent->Fill(float(entry),eGe,lArEvent.edep,lArEvent.PE);
+    if(larEvent->hits.size()>0) ntEvent->Fill(float(entry),eGe,larEvent->hits[0].edep,larEvent->hits[0].PE);
   }
 
 
--- ana/rootlogon.C
+++ ana/rootlogon.C
@@ -1,16 +1,18 @@
 {
-  printf("\n this is rootlogon for bacon \n");
+  printf("\n this is rootlogon for MaGeLAr \n");
   TString arch=gSystem->GetBuildArch();
   gROOT->LoadMacro("util.C");
   cout << " arch is " << arch << endl; 
-  gSystem->AddIncludePath(" -I. -I./obj/");
+  gSystem->AddIncludePath(" -I. -I./obj/ -I$MAGEDIR/io/obj/");
   printf(" include path %s \n\n",gSystem->GetIncludePath());
-  gSystem->AddDynamicPath("/usr/local/lib");
+  gSystem->AddDynamicPath("/usr/local/lib:$MAGEDIR/io/obj");
   printf(" dynamic path %s \n\n",gSystem->GetDynamicPath());
   
   int iload = gSystem->Load("/usr/local/lib/libCLHEP-Random-2.4.1.0.so");
   printf(" loaded libCLHEP-Random-2.4.1.0.so = %i zero is success! \n",iload);
-  iload = gSystem->Load("/home/admin/MGDO/Root/libMGDORoot.so");
+  iload = gSystem->Load("$MGDODIR/Root/libMGDORoot.so");
   printf(" loaded libMGDORoot.so = %i zero is success! \n",iload);
+  iload = gSystem->Load("$MAGEDIR/lib/libLegendRoot.so");
+  printf(" loaded libLegendRoot.so = %i zero is success! \n",iload);
 }
 
